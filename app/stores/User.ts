0b0ac0ed859fbb76769a29845d61e91151620429
wenyetom <havekiez01@gmail.com>
2026-04-15 00:38:27 +0800

    feat: 新增 User Store 變數

--- app/stores/User.ts
+++ app/stores/User.ts
@@ -1,8 +1,11 @@
 export const useUserStore = defineStore("user", {
-  state: () => ({ name: "旅行者" }),
+  state: () => ({ name: "旅行者", playerGender: "male" }),
   actions: {
     changeUserName(newName: string) {
       this.name = newName;
+    },
+    changeGender(newGender: string) {
+      this.playerGender = newGender;
     },
   },
   persist: true,
