0caad0ef4f54fdd388dda9961f0107b296398cf1
wenyetom <havekiez01@gmail.com>
2026-04-17 20:38:36 +0800

    feat(pinia): 新增變數

--- app/stores/User.ts
+++ app/stores/User.ts
@@ -1,11 +1,14 @@
 export const useUserStore = defineStore("user", {
-  state: () => ({ name: "旅行者", playerGender: "male" }),
+  state: () => ({ name: "旅行者", playerGender: "male", defaultTalentLevel: 1 }),
   actions: {
     changeUserName(newName: string) {
       this.name = newName;
     },
     changeGender(newGender: string) {
       this.playerGender = newGender;
+    },
+    changeDefaultTalentLevel(newLevel: number) {
+      this.defaultTalentLevel = newLevel;
     },
   },
   persist: true,
