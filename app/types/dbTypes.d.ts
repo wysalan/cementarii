9ff94b3ff79191b6b06f8f04eccf5ccd7ca30709
wenyetom <havekiez01@gmail.com>
2026-04-17 20:39:51 +0800

    chore: 補齊剩餘的型別

--- app/types/dbTypes.d.ts
+++ app/types/dbTypes.d.ts
@@ -16,11 +16,20 @@ declare global {
     };
 
     type characterStats = {
-      hp: Record<string, string | number>;
-      attack: Record<string, string | number>;
-      defense: Record<string, string | number>;
+      hp: {
+        base: number;
+        curve: string;
+      };
+      attack: {
+        base: number;
+        curve: string;
+      };
+      defense: {
+        base: number;
+        curve: string;
+      };
       substat: Record<string, string>;
-      promotion: Record<string, string>[];
+      promotion: Record<string, number>[];
     };
 
     type characterCosts = {
@@ -31,6 +40,29 @@ declare global {
       };
     };
 
+    type characterTalents = {
+      list: {
+        [key: string]: {
+          name: string;
+          description: string;
+          descriptionBuffed: string;
+          attributes: {
+            labels: string[];
+            parameters: {
+              [key: string]: number[];
+            };
+          };
+        };
+      };
+      costs: Array<{ id: number; name: string; count: number }[]>;
+    };
+
+    type characterPassives = Array<{
+      name: string;
+      description: string;
+      descriptionBuffed: string;
+    }>;
+
     type characterConstellation = {
       name: string;
       list: {
@@ -40,8 +72,48 @@ declare global {
       }[];
     };
 
+    type characterDictionary = {
+      talentKeywords: string[];
+      relatedEffects: Record<string, string>;
+      linkToEffect: Record<string, string>;
+    };
+
     type characterImages = {
       [key: string]: string;
     };
+
+    type characterProfile = {
+      story: {
+        title: string;
+        title2: string;
+        text: string;
+        text2: string;
+        tips: string;
+      }[];
+      voiceover: {
+        title: string;
+        text: string;
+        tasks?: [
+          {
+            type: string;
+            questList: {
+              id: number;
+              questTitle: string;
+              chapterId: number;
+              chapterTitle: string;
+            };
+          },
+        ];
+      }[];
+    };
+
+    type characterOutfits = {
+      id: number;
+      name: string;
+      version: string;
+      sDefault: boolean;
+      description: string;
+      images: Record<string, string>;
+    }[];
   }
 }
