eae984ce84361d454b8d1ff0db31d47eea2dacd2
wenyetom <havekiez01@gmail.com>
2026-04-21 23:32:33 +0800

    feat: 完成聖遺物一覽頁面

---
+++ app/components/Artifact/Block.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+interface ArtifactData {
+  id: number;
+  slug: string;
+  name: string;
+  rarityList: number[];
+  icon: string;
+}
+const { data } = defineProps<{ data: ArtifactData }>();
+
+function getRarityBgColor(maxRarity: number) {
+  if (maxRarity) {
+    switch (maxRarity) {
+      case 1:
+        return "bg-linear-to-b from-#6a6d74 to-#868586";
+      case 2:
+        return "bg-linear-to-b from-#4b6c67 to-#519072";
+      case 3:
+        return "bg-linear-to-b from-#567496 to-#5392b8";
+      case 4:
+        return "bg-linear-to-b from-#5e5789 to-#9c75b7";
+      case 5:
+        return "bg-linear-to-b from-#945c2c to-#b27330";
+    }
+  }
+}
+
+function getRarityRingColor(maxRarity: number) {
+  if (maxRarity) {
+    switch (maxRarity) {
+      case 1:
+        return "ring-#868586";
+      case 2:
+        return "ring-#519072";
+      case 3:
+        return "ring-#5392b8";
+      case 4:
+        return "ring-#9c75b7";
+      case 5:
+        return "ring-#b27330";
+    }
+  }
+}
+</script>
+
+<template>
+  <NuxtLink
+    :to="`/artifact/${data.slug}`"
+    class="flex flex-col justify-center items-center bg-zinc-200 rounded-lg hover:scale-105% hover:ring-2 transition-shadow ease-in-out"
+    :class="getRarityRingColor(data.rarityList[data.rarityList.length - 1] || 1)"
+  >
+    <div class="relative">
+      <NuxtImg
+        :src="'https://assets.wysalan.com/cementarii/relic-icon/' + data.icon + '.webp'"
+        :alt="data.name"
+        width="96"
+        height="96"
+        class="w-25 h-25 lg:w-35 lg:h-35 mask-[linear-gradient(to_bottom,black_85%,transparent_100%)] rounded-t-lg"
+        :class="getRarityBgColor(data.rarityList[data.rarityList.length - 1] || 1)"
+        placeholder
+      />
+    </div>
+    <div class="flex flex-col justify-center items-center h-10 w-full my-1">
+      <p class="break-all w-20 xl:w-30 text-center">{{ data.name }}</p>
+    </div>
+  </NuxtLink>
+</template>
+
+<style scoped></style>
--- app/pages/artifact/index.vue
+++ app/pages/artifact/index.vue
@@ -1,8 +1,100 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import artifactList from "@/data/artifactList.json";
+
+const artifactSearchKeyword = ref("");
+
+const filter = ref<Record<string, string[]>>({
+  rarity: ["3", "4", "5"],
+  raritySelected: [],
+});
+
+const filteredArtifactList = computed(() => {
+  return artifactList
+    .filter((artifact) => {
+      return (
+        (!filter.value.raritySelected?.length ||
+          filter.value.raritySelected.includes(
+            artifact.rarityList[artifact.rarityList.length - 1]!.toString(),
+          )) &&
+        (!artifactSearchKeyword.value ||
+          artifact.name
+            .toLowerCase()
+            .replace(" ", "")
+            .includes(artifactSearchKeyword.value.toLowerCase()) ||
+          artifact.slug
+            .replace("-", "")
+            .includes(artifactSearchKeyword.value.toLowerCase().replace(" ", "")))
+      );
+    })
+    .sort((a, b) => {
+      const maxRarityOfA = a.rarityList[a.rarityList.length - 1] || 1;
+      const maxRarityOfB = b.rarityList[b.rarityList.length - 1] || 1;
+      if (maxRarityOfA !== maxRarityOfB) {
+        return maxRarityOfB - maxRarityOfA;
+      } else {
+        return b.id - a.id;
+      }
+    });
+});
+
+const isFiltered = computed(() => {
+  return filter.value.raritySelected?.length;
+});
+
+const removeAllFilter = () => {
+  filter.value.raritySelected = [];
+};
+</script>
 
 <template>
   <main class="container mx-auto pt-5 pb-10">
-    <p>This is artifact page (Under Construction)</p>
+    <div class="flex flex-col items-center pb-5">
+      <Panel toggleable collapsed class="mx-3 w-75vw md:w-full rounded-xl">
+        <template #header>
+          <div class="flex flex-row items-center gap-2">
+            <p class="font-semibold">篩選</p>
+            <Button
+              label="清除所有篩選條件"
+              rounded
+              class="h-7"
+              v-if="isFiltered"
+              @click="removeAllFilter"
+            />
+          </div>
+        </template>
+        <div class="flex flex-col gap-5">
+          <div class="flex flex-col justify-center items-center gap-2">
+            <p class="text-lg font-semibold">最大星級</p>
+            <div class="flex flex-row gap-2">
+              <SelectButton
+                v-model="filter.raritySelected"
+                :options="filter.rarity"
+                class="flex flex-wrap justify-center items-center"
+                multiple
+              />
+            </div>
+          </div>
+        </div>
+      </Panel>
+      <div class="flex justify-center items-center pt-5">
+        <IconField>
+          <InputIcon class="pi pi-search" />
+          <InputText v-model="artifactSearchKeyword" placeholder="搜尋聖遺物" />
+          <InputIcon
+            class="pi pi-times"
+            @click="artifactSearchKeyword = ''"
+            v-if="artifactSearchKeyword"
+          />
+        </IconField>
+      </div>
+    </div>
+    <div class="flex flex-wrap justify-center gap-5">
+      <ArtifactBlock
+        v-for="(artifact, index) in filteredArtifactList"
+        :key="index"
+        :data="artifact"
+      />
+    </div>
   </main>
 </template>
 
