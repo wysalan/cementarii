96916a46fc784059b1930d34d8bdba5ad951c4d9
wenyetom <havekiez01@gmail.com>
2026-04-25 17:38:31 +0800

    chore: 將 Postgre Adapter 調整為 Neon Adapter

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,8 +1,10 @@
-import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@/generated/prisma/client";
 
 const prismaClientSingleton = () => {
-  const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+  // const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+  const pool = new PrismaNeon({ connectionString: process.env.DATABASE_URL! });
   return new PrismaClient({ adapter: pool });
 };
 
