e33b9e5140eb86afb8a7a5f8391966eda2afc0f2
wenyetom <havekiez01@gmail.com>
2026-04-25 15:41:14 +0800

    refactor: 調整程式碼

--- app/components/LoadingIndicator.vue
+++ app/components/LoadingIndicator.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
-const { progress, isLoading, start, finish, clear } = useLoadingIndicator({
+const { progress, isLoading } = useLoadingIndicator({
   duration: 2000,
   throttle: 200,
-  // This is how progress is calculated by default
-  estimatedProgress: (duration, elapsed) =>
-    (2 / Math.PI) * 100 * Math.atan(((elapsed / duration) * 100) / 50),
 });
 </script>
 
