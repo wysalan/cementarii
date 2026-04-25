850457015d636172b14997177e0fbff152d2df4c
wenyetom <havekiez01@gmail.com>
2026-04-25 15:43:59 +0800

    chore: 移除歡迎元件中的時間顯示

--- app/components/Home/Welcome.vue
+++ app/components/Home/Welcome.vue
@@ -1,26 +1,6 @@
 <script setup lang="ts">
 import { useUserStore } from "@/stores/User";
 const store = useUserStore();
-
-const date = ref("Loading");
-const time = ref();
-let timer: ReturnType<typeof setInterval> | null = null;
-
-onMounted(() => {
-  timer = setInterval(() => {
-    date.value = new Date().toLocaleDateString("zh-TW");
-    time.value = new Date().toLocaleTimeString("zh-TW", {
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: false,
-    });
-  }, 1000);
-});
-
-onUnmounted(() => {
-  if (timer) clearInterval(timer);
-});
 </script>
 
 <template>
@@ -29,11 +9,7 @@ onUnmounted(() => {
       <span>{{ store.name }}</span
       >，歡迎使用 Cementarii
     </p>
-    <div
-      class="bg-zinc-300 rounded-sm w-160px h-28px animate-pulse"
-      v-if="date === 'Loading'"
-    ></div>
-    <p class="text-lg" v-else>{{ date }} {{ time }}</p>
+    <p class="opacity-70">仍在開發中，不代表最終呈現效果</p>
   </div>
 </template>
 
