9db5ad93da6e9086d4385fd06d07a3bfe06e3c64
wenyetom <havekiez01@gmail.com>
2026-04-25 15:41:54 +0800

    chore: 調整首頁 RWD 樣式

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,7 @@
 
 <template>
   <main class="container mx-auto py-5">
-    <div class="grid grid-cols-[repeat(2,1fr)] gap-5">
+    <div class="grid grid-cols-1 lg:grid-cols-[repeat(2,1fr)] gap-5 max-lg:mx-5">
       <div class="flex flex-col gap-5">
         <HomeWelcome />
         <HomeNews />
