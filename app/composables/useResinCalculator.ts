66d54b9103c4a8687d54780bdb537e82048ae13a
wenyetom <havekiez01@gmail.com>
2026-05-03 00:32:21 +0800

    feat: 新增完整版樹脂計算函式

--- app/composables/useResinCalculator.ts
+++ app/composables/useResinCalculator.ts
@@ -19,5 +19,48 @@ export default function useResinCalculator() {
 
     return `${prefix} ${formatTime(nowTime)}`;
   }
-  return { getSimpleResinResult };
+
+  function getFullResinResult(
+    currentResin: number,
+    customTime: { hour: number; minute: number },
+    useCustomTime?: boolean,
+  ) {
+    if (currentResin >= 200) return [{ resin: 200, prefix: "", time: "已回滿" }];
+    const result: { resin: number; prefix: string; time: string }[] = [];
+    const nowTime = new Date();
+    if (useCustomTime) {
+      nowTime.setHours(customTime.hour);
+      nowTime.setMinutes(customTime.minute);
+    }
+    const today = new Date(nowTime);
+    today.setHours(23, 59, 59, 999);
+
+    const resin = Math.max(0, currentResin);
+    const maxResin = 200;
+    const remainResin = maxResin - resin;
+
+    const resinRecoveryMinutes = 8;
+
+    for (let counter = 0; counter < remainResin; counter++) {
+      nowTime.setMinutes(nowTime.getMinutes() + resinRecoveryMinutes);
+      currentResin++;
+      if (!(currentResin % 10) || currentResin === 200) {
+        const prefix = nowTime > today ? "明天" : "今天";
+        result.push({
+          resin: currentResin,
+          prefix: prefix,
+          time: `${formatTime(nowTime)}`,
+        });
+        if (currentResin === 200) {
+          result.unshift({
+            resin: currentResin,
+            prefix: prefix,
+            time: `${formatTime(nowTime)}`,
+          });
+        }
+      }
+    }
+    return result;
+  }
+  return { getSimpleResinResult, getFullResinResult };
 }
