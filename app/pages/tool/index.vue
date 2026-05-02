a990316e27f5ae9b00e5dd4f3856b0cc299756ed
wenyetom <havekiez01@gmail.com>
2026-05-03 00:31:19 +0800

    feat: 工具頁面新增工具

--- app/pages/tool/index.vue
+++ app/pages/tool/index.vue
@@ -8,7 +8,17 @@ useSeoMeta({
 
 <template>
   <main class="container mx-auto pt-5 pb-10">
-    <p>This is tool page (Under Construction)</p>
+    <div class="flex flex-wrap justify-center gap-5">
+      <NuxtLink
+        to="tool/resin-calculator"
+        class="flex flex-col justify-center items-center w-40 h-45 bg-zinc-200 hover:bg-zinc-400 rounded-xl transition-colors"
+      >
+        <div>
+          <NuxtImg src="https://assets.wysalan.com/cementarii/item-icon/UI_ItemIcon_211.webp" />
+        </div>
+        <p>樹脂計算器</p>
+      </NuxtLink>
+    </div>
   </main>
 </template>
 
